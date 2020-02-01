4924ed4e0d7548650e6714633afe219e0ead9b51
Nonnisi <daniel@nonit.me>
2020-02-01 19:47:33 +0000

    Code stuff

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -13,6 +13,24 @@ public class CameraScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+             
+ RaycastHit hit;
+         
+ if( Physics.Raycast(ray, out hit, 100) )
+     Debug.DrawLine(ray.origin, hit.point, Color.green);
+ 
+             // Allows zooming in and out via the mouse wheel
+             if(Input.GetAxis("Mouse ScrollWheel") < 0)
+             {
+                 transform.Translate(0, 1, 0, Space.World);
+                 transform.position = new Vector3 (transform.position.x ,Mathf.Clamp(transform.position.y,6,10), transform.position.z) ;                    
+             }                
+             if(Input.GetAxis("Mouse ScrollWheel") > 0)
+             {
+                 transform.Translate(0,-1,0, Space.World);
+                 transform.position = new Vector3 (hit.point.x ,Mathf.Clamp(transform.position.y,6,10), hit.point.z);
+             }
+
     }
 }
--- Assets/Scripts/MotelScript.cs
+++ Assets/Scripts/MotelScript.cs
@@ -4,11 +4,48 @@ using UnityEngine;
 
 public class MotelScript : MonoBehaviour
 {
+    public int CoolDown = 60;
+    public GameObject monster;
     private List<MonsterScript> monstersInMotel;
+    private int Timer = -1;
 
     void Start() 
     {
         monstersInMotel = new List<MonsterScript>();
+    }
+
+    private void Update() 
+    {
+        if(Timer > -1 && Timer < CoolDown)
+        {
+            Timer++;
+        }
+        if(Timer >= CoolDown)
+        {
+            Timer = -1;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(Timer > -1) {return;}
+
+        var monster = other.gameObject.GetComponent<MonsterScript>();
+        if(monster == null ) {return;} //ITS NOT A MONSTER
+        if(monster.isInMotel) {return;} //ITS ALLREADY IN THE MOTEL
+
+        monster.isInMotel = true;
+        monster.canMove = false;
+        monstersInMotel.Add(monster);
+
+        //THE HOTEL IS FULL GET TO THE MAKING
+        if(monstersInMotel.Count >= 2)
+        {
+            MakeBaby(monstersInMotel[0], monstersInMotel[1]);
+            foreach(MonsterScript motelMonster in monstersInMotel) { motelMonster.isInMotel = false; monster.canMove = true; }
+            monstersInMotel.Clear();
+            Timer = 0;
+        }
     }
 
     public void MakeBaby( MonsterScript parent1, MonsterScript parent2 )
@@ -19,17 +56,20 @@ public class MotelScript : MonoBehaviour
 
         parts = Utils.Shuffle<PartScript>(parts); //Randomize parts
 
+        var baby = Instantiate(monster);
+
         //Attach bodyparts
-        foreach (Transform child in this.transform)
+        foreach (Transform child in baby.transform)
         {
             var part = child.GetComponent<PartScript>();
+            if(part == null) {Debug.Log("MMMOOXX");}
             var type = part.type;
 
             foreach( PartScript parentPart in parts )
             {
                 if( parentPart.type.Equals(type) )
                 {
-                    child = parentPart;
+                    //child = parentPart;
                     break;
                 }
             }
