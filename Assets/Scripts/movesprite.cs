20b7e6747573e2da650469532b505b8f7e3e9e11
alastairpeat <ali_peat_01@hotmail.co.uk>
2020-02-01 13:30:05 +0000

    for dragging the characters around the scene
    add a rigidbody 2D to the game object

---
+++ Assets/Scripts/movesprite.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movesprite : MonoBehaviour
+{
+  private float StartPosX;
+  private float StartPosY;
+  private bool isBeingHeld = false;
+  
+  void Update()
+  {
+      if(isBeingHeld == true)
+      {
+      Vector3 mousePos;
+      mousePos = Input.mousePosition;
+      mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+
+      this.gameObject.transform.localPosition = new Vector3(mousePos.x - StartPosX, mousePos.y - StartPosY, 0);
+      }
+
+  }
+
+  private void OnMouseDown()
+  {
+      if(Input.GetMouseButtonDown(0))
+      {
+      Vector3 mousePos;
+      mousePos = Input.mousePosition;
+      mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+
+      StartPosX = mousePos.x - this.transform.localPosition.x;
+      StartPosY = mousePos.y - this.transform.localPosition.y;
+
+
+      isBeingHeld = true;
+  }
+  }
+  private void OnMouseUp()
+  {
+      isBeingHeld = false;
+
+  }}
