041df5b26da569284985f200d4ae334a9eb70ff6
Nonnisi <daniel@nonit.me>
2020-02-01 12:14:19 +0000

    Init

---
+++ Assets/Scripts/PartScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PartScript : MonoBehaviour
+{
+    public int PartImage;
+    public int dominance = 100; //How lightly is this part to show up v being recessive
+    public float heightScale = 1; // How high is the image
+    public float widthScale = 1; // How wide is the image
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
