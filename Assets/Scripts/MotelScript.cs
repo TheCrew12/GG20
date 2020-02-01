e1b62ef1eaf987309d5513fb118995942dd2d709
Nonnisi <daniel@nonit.me>
2020-02-01 14:52:16 +0000

    More stuff

--- Assets/Scripts/MotelScript.cs
+++ Assets/Scripts/MotelScript.cs
@@ -4,12 +4,35 @@ using UnityEngine;
 
 public class MotelScript : MonoBehaviour
 {
+    private List<MonsterScript> monstersInMotel;
+
+    void Start() 
+    {
+        monstersInMotel = new List<MonsterScript>();
+    }
+
     public void MakeBaby( MonsterScript parent1, MonsterScript parent2 )
     {
         List<PartScript> parts = new List<PartScript>();
         parts.AddRange(parent1.GetBodyParts());
         parts.AddRange(parent2.GetBodyParts());
 
-        
+        parts = Utils.Shuffle<PartScript>(parts); //Randomize parts
+
+        //Attach bodyparts
+        foreach (Transform child in this.transform)
+        {
+            var part = child.GetComponent<PartScript>();
+            var type = part.type;
+
+            foreach( PartScript parentPart in parts )
+            {
+                if( parentPart.type.Equals(type) )
+                {
+                    child = parentPart;
+                    break;
+                }
+            }
+        }
     }
 }
