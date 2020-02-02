0dd7353917e8026e31429a8964f073d23c7c99e4
Shitao Fan <Shitao.Fan15@gmail.com>
2020-02-02 14:48:08 +0000

    1

---
+++ Assets/TurnOnCharacter.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnOnCharacter : MonoBehaviour
+{
+    // Start is called before the first frame update
+   
+   public GameObject monster;
+   private bool toggle = true;
+    void Start()                                                        
+    {
+    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+            if(Input.GetKeyDown(KeyCode.Space))
+            {
+                if(toggle)
+                {
+                     monster.SetActive(!monster.activeSelf);  
+                     toggle = false; 
+                }
+                else{
+                    monster.SetActive(monster.activeSelf);
+                     toggle = true;   
+                }
+            }      
+    }
+    
+}
