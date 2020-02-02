99fe75833c3281e5695a16eb2469d13fabfc8328
Chrxstian <Chrxstian@users.noreply.github.com>
2020-02-02 12:57:30 +0000

    added files

---
+++ Assets/Scripts/Sound.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine.Audio;
+using UnityEngine;
+
+/********* THIS CODE IS BY BRACKEYS (brackeys.com) ********/
+
+[System.Serializable]
+public class Sound {
+
+	public string name;
+
+	public AudioClip clip;
+
+	[Range(0f, 1f)]
+	public float volume = .75f;
+	[Range(0f, 1f)]
+	public float volumeVariance = .1f;
+
+	[Range(.1f, 3f)]
+	public float pitch = 1f;
+	[Range(0f, 1f)]
+	public float pitchVariance = .1f;
+
+	public bool loop = false;
+
+	[Range(0f, 1f)]
+	public float spatialBlend = 0f;//float for spacial blend
+
+	public AudioMixerGroup mixerGroup;
+
+	[HideInInspector]
+	public AudioSource source;
+
+}
